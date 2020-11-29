e0371850a844e430e3700b08a1425a39e43839b2
327jwolf <327jwolf@gmail.com>
2020-11-28 21:16:14 -0600

    Same damned problem

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -463,13 +463,13 @@ domIsReady (function() {
                     const createdAtInput = cardcontainer.querySelector('input[name=createdAt]');
                     let now = new Date();
                     createdAtInput.value = now; 
-                    if(e.target.parentElement.nextElementSibling){
+                    if(e.target.parentElement.nextElementSibling !== null){
                         const box = e.target.parentElement.nextElementSibling.getBoundingClientRect();
-                    console.log(box)
-                    subform.style.position = 'absolute';
-                    subform.style.top = `${window.scrollY + box.top - 450}px`;
-                    subform.style.left = `${((window.innerWidth - box.width) / 2)*.55}px`;
-                    subform.style.zIndex = '5';
+                        console.log(box)
+                        subform.style.position = 'absolute';
+                        subform.style.top = `${window.scrollY + box.top - 450}px`;
+                        subform.style.left = `${((window.innerWidth - box.width) / 2)*.55}px`;
+                        subform.style.zIndex = '5';
                     }
                     
 
